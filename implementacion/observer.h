b63c6e42fbbef30d4037524ce8f1aa838c80b73a
gabriel <gabrielcorbot@gmail.com>
2022-02-28 11:13:01 -0500

    Definicion de atributos y metodos basicos para cada clase

--- implementacion/observer.h
+++ implementacion/observer.h
@@ -1,1 +1,11 @@
 //Suscriptor
+#ifndef OBSERVER_H_
+#define OBSERVER_H_
+
+//MET: Update(), como mínimo este metodo que implementa de una interfaz.
+
+typedef struct{
+
+}observer;
+
+#endif
--- implementacion/subject.h
+++ implementacion/subject.h
@@ -1,1 +1,14 @@
 //Notificador o publicador
+#ifndef SUBJECT_H
+#define SUBJECT_H
+
+//ATR: Lista de suscriptores
+//MET: Agregar suscriptor
+//MET: Eliminar suscriptor
+//MET: Notificar suscriptores de la lista
+
+typedef struct{
+
+}subject;
+
+#endif
