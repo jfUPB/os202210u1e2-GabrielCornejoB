92f7062bc53bb6b6b87cff50791166f8bddf7196
gabriel <gabrielcorbot@gmail.com>
2022-03-03 15:37:39 -0500

    faltaron unas cosas jaja

---
+++ ejemplo/player.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "player.h"
+#include "dt.h"
+
+static void _destroy(Player *this){
+    this->observer->destroyOb(this->observer);
+    if(this != NULL){
+        free(this);
+        this = NULL;
+    }
+}
+static Location _getLocation(Player *this){
+    printf("Player: getLocation(): %d\n",this->location);
+    return this->location;
+}
+//Tal vez si realizo herencia y coloco el atributo observer de primero en la clase jugador pueda
+//omitir esto
+static void _enterField(Player *this, Dt *dt){
+    printf("Player: EnterField()\n");
+    dt->addObserver(dt, this->observer);  
+}
+static void _handleDtEvent(Player *this, Dt *dt){
+    //printf("Jugador: %s, se reubicará por orden de: %s\n",this->nombre,dt->nombre);
+    this->location = 2;
+}
+
+static void _update(Player *this, int state, void *subject){
+    printf("Player: update()\n");
+    _handleDtEvent(this, (Dt*) subject);
+}
+Player *player_new(char *playerName, int default_location){
+    Player *this = (Player*)malloc(sizeof(*this));
+    this->name = playerName;
+    this->location = default_location;
+    this->destroy = _destroy;
+    this->getLocation = _getLocation;
+    this->enterField = _enterField;
+    this->observer = observer_new(this, (void (*)(void*, int, void*))_update);
+    return this;
+}
---
+++ ejemplo/player.h
@@ -0,0 +1,23 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+#include "../implementacion/subject.h"
+#include "../implementacion/observer.h"
+#include "dt.h"
+
+typedef enum _location{
+    UP = 0,
+    MID = 1,
+    BACK = 2
+}Location;
+
+typedef struct _player{
+    char *name;
+    Location location;
+    Location (*getLocation)(struct _player *);
+    void (*enterField)(struct _player *, Dt*);
+    void (*destroy)(struct _player *);
+    Observer * observer;
+}Player;
+
+Player *player_new(char *, int);
+#endif
